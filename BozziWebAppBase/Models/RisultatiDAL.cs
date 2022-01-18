6f8b734c3218a888dce9652e0ceb7620eed7ca43
Marcello Bozzi <bozzimarcello@gmail.com>
2022-01-18 18:31:43 +0100

    Aggiungere i file di progetto.

---
+++ BozziWebAppBase/Models/RisultatiDAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace BozziWebAppBase.Models
+{
+    public class RisultatiDAL
+    {
+        public static List<Risultato> GetAllRisultati()
+        {
+
+            List<Risultato> lista = new List<Risultato>();
+
+            lista.Add(new Risultato { Id = 1, Titolo = "Primo", Descrizione = "Primo record" });
+            lista.Add(new Risultato { Id = 2, Titolo = "Secondo", Descrizione = "Secondo record" });
+            lista.Add(new Risultato { Id = 3, Titolo = "Terzo", Descrizione = "Terzo record" });
+
+            return lista;
+        }
+    }
+}
---
+++ BozziWebAppBase/Pages/Search.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BozziWebAppBase.Models;
+
+namespace BozziWebAppBase.Pages
+{
+    public class SearchModel : PageModel
+    {
+        public string Q { get; set; }
+        public List<Risultato> Results { get; set; }
+        public void OnGet(string q)
+        {
+            Q = q;
+            Results = RisultatiDAL.GetAllRisultati();
+        }
+    }
+}
